ab8ee41683708ce1b681dd2b49b5feff183d0770
Richie <richiejoeyoung@gmail.com>
2017-10-24 19:04:14 -0400

    add missing comma

--- lib/yacs/auth/login.rb
+++ lib/yacs/auth/login.rb
@@ -2,7 +2,7 @@ module Yacs::Auth::Login
   class << self
     def sign_in user
       token = Token.encode user_id: user.id
-      Session.save token { valid: true }
+      Session.save token, { valid: true }
       token
     end
   end
