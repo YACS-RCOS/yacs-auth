0901661c2da78d4972097f8b3a83da0a44a71afc
Richie <richiejoeyoung@gmail.com>
2017-10-24 19:07:52 -0400

    move sign_in to Yacs::Auth

--- lib/yacs/auth.rb
+++ lib/yacs/auth.rb
@@ -8,6 +8,12 @@ module Yacs
       attr_accessor :config
     end
 
+    def self.sign_in user
+      token = Token.encode user_id: user.id
+      Session.save token, { valid: true }
+      token
+    end
+
     def self.config
       @config ||= Config.new
     end
